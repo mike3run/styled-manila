d6c88569a65ac2756a2b00294856ee3f189a7be3
Miguel Palau Zarza <mpalauzarza@gmail.com>
2018-07-08 11:27:08 -0500

    testing out our first component

--- src/stdBackground/stdBackground.js
+++ src/stdBackground/stdBackground.js
@@ -1,7 +1,14 @@
 import { css } from 'styled-components'
 
+/**
+ * Normalize background properties useful when changing
+ * background image via server side on the actual html markup
+ * @returns {CSS}
+ */
 const stdBackground = css`
-
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
 `
 
 export default stdBackground
--- src/stdBackground/stdBackground.test.js
+++ src/stdBackground/stdBackground.test.js
@@ -6,15 +6,12 @@ import stdBackground from './stdBackground'
 
 const Background = styled.div`
   ${stdBackground}
-  background-position: center;
-  background-size: cover;
-  background-repeat: no-repeat;
 `
 
 test('it works', () => {
   const tree = renderer.create(<Background />).toJSON()
   expect(tree).toMatchSnapshot()
-  expect(tree).toHaveStyleRule('backgroundPosition', 'center')
-  expect(tree).toHaveStyleRule('backgroundSize', 'cover')
-  expect(tree).toHaveStyleRule('backgroundRepeat', 'no-repeat')
+  expect(tree).toHaveStyleRule('background-position', 'center')
+  expect(tree).toHaveStyleRule('background-size', 'cover')
+  expect(tree).toHaveStyleRule('background-repeat', 'no-repeat')
 })
