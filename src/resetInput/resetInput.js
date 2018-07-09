8f00d4e45f691b0ccfb572f2f5efbfcde6a528b3
Miguel Palau Zarza <mpalauzarza@gmail.com>
2018-07-08 21:28:32 -0500

    resetInput

---
+++ src/resetInput/resetInput.js
@@ -0,0 +1,16 @@
+import { css } from 'styled-components'
+
+const resetBtn = css`
+  appearance: none;
+  border: none;
+  border-radius: 0;
+  background: none;
+  margin: 0;
+  padding: 0;
+  font: inherit;
+  line-height: inherit;
+  color: inherit;
+  outline: none;
+`
+
+export default resetBtn
